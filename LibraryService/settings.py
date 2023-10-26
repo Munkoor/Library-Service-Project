4ddbe2298a47bb5981bcbb6b61a109918da7ed6e
Munkoor <eee1lvlboss@gmail.com>
2023-10-26 20:22:17 +0300

    Added my apps in installed apps in settings.py

--- LibraryService/settings.py
+++ LibraryService/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user",
+    "library_app"
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 
 # Internationalization
